09a94b425a44201e3efb5e30c24eaa3a73b73ea9
OlgaVlasova <vlasovaolga8@yandex.ru>
2023-11-12 02:50:04 +0300

    work-3 fix game

--- game.js
+++ game.js
@@ -1,83 +1,83 @@
 let cvs = document.querySelector("#flappybird");
 let ctx = cvs.getContext("2d");
-let bird = document.createElement('img');
-bird.src = "images/bird.png";
-let bg = document.createElement('img');
-bg.src = "images/bg.png";
-let pipeBottom = document.createElement('img');
-pipeBottom.src = "images/pipeBottom.png";
+let cat = document.createElement('img');
+cat.src = "images/CatMini.png";
+
+let burgerMini = document.createElement('img');
+burgerMini.src = "images/burgerMini.png";
 
 
-let fg = document.createElement('img');
-fg.src = "images/fg.png";
+let grav = 0.2;
+let change = 0;
+let floor = cvs.height  - burgerMini.height
 
-let grav = 0.3;
-let change = 5;
-
-let gap = 110;
-let pipes_x = [cvs.clientWidth, cvs.width + 150];
-let pipes_y = [0, -100];
-
-let x = 280;
-let y = 0;
+let pipes_x = [cvs.width];
+let pipes_y = [floor];
 
 let xPos = 50;
-let yPos = 250;
+let yPos = floor;
+let jumpHeight = 7;
+let delta = 0.4;
 
 let score = 0;
+let speed = 2.1;
 
 document.addEventListener('keydown', function(){
-    change = 4;
+    if(change > 0 || yPos != floor)
+        return;
+    change = jumpHeight;
 })
 
 
-
 function draw() {
-    ctx.drawImage(bg, 0, 0);
     for (i = 0; i < pipes_x.length; i++) {
-        ctx.drawImage(pipeUp, pipes_x[i], pipes_y[i]);
-        ctx.drawImage(pipeBottom, pipes_x[i], pipes_y[i] + pipeUp.height + gap);
-        ctx.drawImage(fg, 0, 400);
+        ctx.clearRect(0,0, cvs.width, cvs.height); //очистить холст после каждого фрэйма, чтобы не оставались уже прорисованные изображения
+        ctx.drawImage(burgerMini, pipes_x[i], floor);
         
-        pipes_x[i] = pipes_x[i] - 2;
-        
-        
+        pipes_x[i] = pipes_x[i] - speed;
+
+        if(pipes_x[i] < -150){
+            pipes_x = [cvs.width];
+        }
         
         if (pipes_x[i] === 50) {
             pipes_x.push(pipes_x[pipes_x.length - 1] + 250);
-            pipes_y.push(Math.floor(Math.random() * pipeUp.height) - pipeUp.height);
+            pipes_y.push(floor);
         }
-        if (xPos + bird.width >= pipes_x[i] 
-            && xPos <= pipes_x[i] + pipeUp.width 
-            && (yPos <= pipes_y[i] + pipeUp.height && yPos + bird.height >= cvs.height - fg.height))
+        if (xPos + cat.width >= pipes_x[i] 
+            && xPos <= pipes_x[i] + 4 
+            && yPos >= pipes_y[i] - jumpHeight + delta )
         {
             pipes_x = [cvs.width];
-            pipes_y = [0];
+            pipes_y = [floor];
             score = 0;
-            xPos = 10;
-            yPos = 150;
-            change = 5;
-        }
-        if (pipes_x[i] < 5) {
-            score++;
-            }
-
-        if (grav <= change) {
-            bird.src = 'images/bird_up.png'
-        }
-        else{
-            bird.src = 'images/bird_down.png'
+            xPos = 50;
+            yPos = floor;
+            change = 0;
+            speed = 2.1;
         }
 
+        score++;
+
+        if(score == speed*1000)
+        {
+            speed += 0.3;
+        }
         
     };
+  
     
-    ctx.drawImage(bird, xPos, yPos);
-    yPos = yPos - change;
+    ctx.drawImage(cat, xPos, yPos);
+    if(yPos - change >= floor){
+        yPos = floor;
+    }else{
+        yPos = yPos - change;
+    }
+    
     change = change - grav;
     ctx.fillStyle = "#000";
     ctx.font = "24px Arial";
-    ctx.fillText("Счет: " + score, 10, cvs.height - 20   );
+    ctx.fillText("Счет: " + score, 10, cvs.height - 20);
     requestAnimationFrame(draw);
 }
 
